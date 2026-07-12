e2fe765aa544efbc9146a3ab044f4d62482b1222
Sathwika1522 <sathwikathuraka6@gmail.com>
2026-07-12 19:29:40 +0530

    add countdigits.py

--- countdigits.py
+++ countdigits.py
@@ -1,7 +1,6 @@
-num=int(input("Enter a number:"))
+num = int(input("Enter a number:")
 count = 0
-while num>0:
-	count = count+1
+while num >0:
 	num = num//10
+	count += 1
 print("Number of digits=",count)
-	
