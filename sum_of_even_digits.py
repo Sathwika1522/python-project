480f113f887c179ffca73915d1322493c7d35d1f
Sathwika1522 <sathwikathuraka6@gmail.com>
2026-07-18 18:42:45 +0530

    add sum_of_even_digits.py

---
+++ sum_of_even_digits.py
@@ -0,0 +1,8 @@
+num = int(input("Enter first number:"))
+even_sum = 0
+while num > 0:
+    digit = num % 10
+  if digit % 2 == 0:
+    even_sum ++ digit 
+ sum = num // 10
+print("Sum of even digits:",even_sum)
