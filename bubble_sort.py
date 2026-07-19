7e2be4c4a4c6987878d15437a58f4818f8584212
Sathwika1522 <sathwikathuraka6@gmail.com>
2026-07-19 21:33:47 +0530

    add bubble_sort.py

---
+++ bubble_sort.py
@@ -0,0 +1,8 @@
+numbers = list(map(int, input("Enter numbers: ").spilt()))
+n = len(numbers)
+for i in range(n):
+  for j in range(0,n - i - 1):
+    if numbers[j] > numbers[j + 1]:
+      numbers[j], numbers[j + 1] = numbers[j + 1],numbers[j]
+      print("Sorted list:", numbers)
+      
